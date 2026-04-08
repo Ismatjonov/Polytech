288d095e1f71ba2af0a8a463254643ced9f9e1e0
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-04-08 22:33:58 +0500

    Uploaded Lecture 10

---
+++ Lecture10/Работа с текстом/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Работа_с_текстом
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("C:\\main.txt", Encoding.UTF8);
+            StreamWriter sw = new StreamWriter("C:\\newmain.txt", true, Encoding.UTF8);
+
+
+
+            try
+            {
+                string t = sr.ReadToEnd();
+                sw.WriteLine(t);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                sw.Close();
+            }
+        }
+    }
+}

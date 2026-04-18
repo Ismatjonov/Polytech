0dd42aedf93a03e2f5565d6295d30fc219bb6131
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-04-18 22:54:23 +0500

    Uploaded today's new theme (Operators)

---
+++ Operator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Operator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pereg a = new Pereg(25);
+        Pereg b = new Pereg(45);
+        int summa = a + b;
+        Console.WriteLine(summa);
+    }
+}
+
+class Pereg
+{
+    private int adad;
+
+    public Pereg(int a)
+    {
+        adad = a;
+    }
+
+    static public int operator +(Pereg a, Pereg b)
+    {
+        return a.adad + b.adad;
+    }
+}

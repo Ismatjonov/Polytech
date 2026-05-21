415d45e76660bab9b0dfc13f0b95e5f6013cd86b
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-05-21 23:13:14 +0500

    Add files via upload

---
+++ Olymp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Olymp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        List<string> numbers = new List<string>();
+        numbers.AddRange(input.Split(" "));
+        
+        
+    }
+}
---
+++ Olymp3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Olymp3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] vars = input.Split(" ");
+        List<int> numbers = new List<int>();
+        numbers.Add(int.Parse(vars[0]));
+        numbers.Add(int.Parse(vars[1]));
+    }
+}
